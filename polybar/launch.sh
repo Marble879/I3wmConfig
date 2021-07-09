7a441ac261c4b97cb0bfa491448fa1ccf37abf55
Marble879 <younisakel879@gmail.com>
2021-07-09 18:10:13 +0200

    Modify selected workspace underline in polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
 
