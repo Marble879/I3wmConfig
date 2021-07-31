ae3a70b743c102c9bd1d6c20b7eebc11ee27d41e
Marble879 <younisakel879@gmail.com>
2021-07-31 09:44:05 +0200

    Add rofi config

---
+++ rofi/powermenu.sh
@@ -0,0 +1,33 @@
+# Code adapted from: https://github.com/prdanelli/dotfiles
+#!/usr/bin/env bash
+
+dir="~/.config/rofi"
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Options
+shutdown="Shutdown"
+reboot="Restart"
+lock="Lock"
+suspend="Sleep"
+logout="Logout"
+
+# Variable passed to rofi
+options="$suspend\n$logout\n$lock\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Power Options" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+			systemctl poweroff
+			;;
+    $reboot)
+			systemctl reboot
+			;;
+    $suspend)
+			mpc -q pause
+			systemctl suspend
+			;;
+    $logout)
+		  bspc quit
+      ;;
+esac
+
